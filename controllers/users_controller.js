8e86b756dc37b72957f105b97af73cbdaee0b97d
Baishali Datta <>
2020-06-19 22:55:43 +0530

    Create Sign In and Sign Up forms

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,3 +7,27 @@ module.exports.profile = function(req,res){
         title: "User Profile"
     });
 }
+
+//action for rendering Sign Up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign up"
+    })
+}
+
+//action for rendering Sign In page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+
+}
+
+//sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    
+}
